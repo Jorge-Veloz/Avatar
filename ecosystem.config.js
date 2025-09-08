42f9925e14adea021d6f8eb969b11d85542656f3
root <root@fci-deepseek.utm.edu.ec>
2025-09-08 14:42:00 -0500

    Mejoras generales

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+     {
+      name: 'prod-asistente-energetico',
+      namespace: 'asistente-energetico',
+      script: 'App.py',
+      cwd: '/home/asistente-energetico',
+      //interpreter: '/usr/local/bin/python3.10',
+      //watch: true
+    }
+  ]
+};
